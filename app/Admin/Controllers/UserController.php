a9dcc0f7ac4698c8e75ef85fc3d16ce9cd9db2e9
root <root@pcsrv.lan>
2018-03-02 15:51:35 +0800

    a

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -136,7 +136,7 @@ class UserController extends Controller
 
                 $form->text('name')/*->rules('required')*/;
                 $form->email('email')->rules('required');
-                $form->phone('phone');
+                $form->text('phone');
                 $form->display('created_at');
                 $form->display('updated_at');
 
