590c23da750f7918363c890af5177204d28bcb96
root <root@pcsrv.lan>
2018-03-04 21:32:41 +0800

    a

--- app/User.php
+++ app/User.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword;
 
-class User extends Authenticatable
+class User extends CanResetPassword
 {
     use Notifiable;
 
